cc6a59341ea1dc583f4f86cc374e839398ce22fe
ankqus <67884191+ankqus@users.noreply.github.com>
2023-02-24 19:12:27 +0545

    Create script.js

---
+++ script.js
@@ -0,0 +1,37 @@
+const cache = {};
+
+function checkHistory(url) {
+  if (cache[url]) {
+    return Promise.resolve(cache[url]);
+  }
+  
+  return fetch(url)
+    .then(response => {
+      if (response.ok) {
+        cache[url] = true;
+      } else {
+        cache[url] = false;
+      }
+      return cache[url];
+    })
+    .catch(error => {
+      console.error(error);
+      cache[url] = false;
+      return false;
+    });
+}
+
+const form = document.querySelector('form');
+const historyList = document.querySelector('.history-list');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const url = document.querySelector('#input-url').value;
+  checkHistory(url)
+    .then(result => {
+      const historyItem = document.createElement('li');
+      historyItem.classList.add('history-item');
+      historyItem.textContent = `${url} - ${result ? 'Exists' : 'Does not exist'}`;
+      historyList.appendChild(historyItem);
+    });
+});
