f9d3a67f2d3f6d61afd91f29db986971524f2095
ankqus <67884191+ankqus@users.noreply.github.com>
2023-02-24 20:10:57 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-// Get the check button and result div
 const checkBtn = document.getElementById("check-btn");
 const resultDiv = document.getElementById("result");
 
-// Check if the website has been visited before
 const isVisited = localStorage.getItem("visited");
 
 if (isVisited) {
@@ -11,27 +9,31 @@ if (isVisited) {
   resultDiv.textContent = "Busted!";
 }
 
-// Add a click event listener to the check button
 checkBtn.addEventListener("click", () => {
   const urlInput = document.getElementById("url");
   const url = urlInput.value;
 
-  // Check if the website exists by sending a HEAD request
-  fetch(url, { method: "HEAD" })
-    .then(response => {
-      if (response.ok) {
-        // If the website exists, set the background color to red and white and display "Busted!" text
-        document.body.style.background = "linear-gradient(45deg, #ff0000, #ffffff)";
-        resultDiv.textContent = "Busted!";
-        // Store the visited status in local storage
-        localStorage.setItem("visited", true);
-      } else {
-        // If the website does not exist, display an error message
-        resultDiv.textContent = "Website does not exist";
-      }
-    })
-    .catch(error => {
-      // If there is an error, display an error message
-      resultDiv.textContent = "An error occurred while checking the website";
-    });
+  caches.match(url).then(response => {
+    if (response) {
+      // If the website exists in the cache, set the background color to red and white and display "Busted!" text
+      document.body.style.background = "linear-gradient(45deg, #ff0000, #ffffff)";
+      resultDiv.textContent = "Busted!";
+      localStorage.setItem("visited", true);
+    } else {
+      fetch(url).then(response => {
+        if (response.ok) {
+          caches.open("website-cache").then(cache => {
+            cache.put(url, response);
+          });
+          document.body.style.background = "linear-gradient(45deg, #ff0000, #ffffff)";
+          resultDiv.textContent = "Busted!";
+          localStorage.setItem("visited", true);
+        } else {
+          resultDiv.textContent = "Website does not exist";
+        }
+      }).catch(error => {
+        resultDiv.textContent = "An error occurred while checking the website";
+      });
+    }
+  });
 });
